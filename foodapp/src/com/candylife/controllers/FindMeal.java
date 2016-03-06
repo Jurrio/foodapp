c6fab12a17a7cd7c00cb9fece7301d7693a579e3
Jurrio <jurimik@yandex.ua>
2016-03-06 04:55:59 +0200

    create @Webservlet FindMeal

---
+++ foodapp/src/com/candylife/controllers/FindMeal.java
@@ -0,0 +1,23 @@
+package com.candylife.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.candylife.service.MealService;
+
+@WebServlet("/findMeal")
+public class FindMeal extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String search = req.getParameter("search");
+		
+		MealService.find(search);
+	}
+
+}
