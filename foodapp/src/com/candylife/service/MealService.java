9d72dfee54b351c55cbf40f4a47c925f07b3f196
Jurrio <jurimik@yandex.ua>
2016-03-07 01:21:02 +0200

    remove switch, add catch for double

--- foodapp/src/com/candylife/service/MealService.java
+++ foodapp/src/com/candylife/service/MealService.java
@@ -1,5 +1,6 @@
 package com.candylife.service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -9,25 +10,18 @@ import com.candylife.model.Desert;
 import com.candylife.model.MainCourse;
 import com.candylife.model.Meal;
 import com.candylife.model.Soup;
+import com.candylife.util.MealUtil;
 
 public class MealService {
 	public static void add(String title, String description, String type, String available, String price, String owner, String time){
 		boolean isAvailable = available != null;
-		double dPrice = Double.parseDouble(price);
-		Date date = new Date(); //TODO: date format in html
-		Meal meal = null;
-		switch (type) {
-		case "soup":
-			meal = new Soup(title, description, isAvailable, dPrice, owner, date);
-			break;
-		case "mainCourse":
-			meal = new MainCourse(title, description, isAvailable, dPrice, owner, date);
-			break;
-		case "desert":
-			meal = new Desert(title, description, isAvailable, dPrice, owner, date);
-		default:
-			break;
+		try {
+			double dPrice = Double.parseDouble(price);
+		} catch (NumberFormatException e) {
+			//TODO: add anything :^)
 		}
+		Date date = new Date(); //TODO: date format in html
+		Meal meal = MealUtil.create(type);
 		new MealRepository().create(meal);
 	}
 	
