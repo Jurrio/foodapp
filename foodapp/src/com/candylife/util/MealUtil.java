0a7043d398a7f592596a8c5a73148e14424ab2b9
Jurrio <jurimik@yandex.ua>
2016-03-07 01:22:21 +0200

    add comments

--- foodapp/src/com/candylife/util/MealUtil.java
+++ foodapp/src/com/candylife/util/MealUtil.java
@@ -9,11 +9,11 @@ public class MealUtil {
 	public static Meal create(String type) {
 		switch (type) {
 		case "soup":
-			return new Soup();
+			return new Soup();//TODO: add setters
 		case "mainCourse":
-			return new MainCourse();
+			return new MainCourse();//TODO: add setters
 		case "desert":
-			return new Desert();
+			return new Desert();//TODO: add setters
 		default:
 			return null;
 		}
