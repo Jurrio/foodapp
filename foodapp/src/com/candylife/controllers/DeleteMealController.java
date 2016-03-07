a196c6380770da27587431e87a69a69f3f1c90ca
Jurrio <jurimik@yandex.ua>
2016-03-07 10:22:33 +0200

    add constants in DeleteMealController

--- foodapp/src/com/candylife/controllers/DeleteMealController.java
+++ foodapp/src/com/candylife/controllers/DeleteMealController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.candylife.constants.WebPage;
 import com.candylife.service.MealService;
 
 @WebServlet("/deleteMeal")
@@ -15,9 +16,9 @@ public class DeleteMealController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String title = req.getParameter("title");
-		String description = req.getParameter("description");
-		String price = req.getParameter("price");
+		String title = req.getParameter(WebPage.TITLE);
+		String description = req.getParameter(WebPage.DESCRIPTION);
+		String price = req.getParameter(WebPage.PRICE);
 		
 		MealService.delete(title, description, price);
 	}
