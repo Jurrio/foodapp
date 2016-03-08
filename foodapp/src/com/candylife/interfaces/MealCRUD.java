d5ad0926db58ce2a1f1d730d7ddd7ef29cc28a04
Jurrio <jurimik@yandex.ua>
2016-03-08 21:04:05 +0200

    add type to List in method read()

--- foodapp/src/com/candylife/interfaces/MealCRUD.java
+++ foodapp/src/com/candylife/interfaces/MealCRUD.java
@@ -7,7 +7,7 @@ import com.candylife.model.Meal;
 public interface MealCRUD {
 	void create(Meal meal);
 	Meal read(int index);
-	List read();
+	List<Meal> read();
 	void update(int index, Meal meal);
 	void delete(Meal meal);
 	void delete(int index); //id
