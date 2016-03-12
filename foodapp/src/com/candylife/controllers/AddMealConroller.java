0396c356bfae683dfd17674397c715f5daa6be40
Jurrio <jurimik@yandex.ua>
2016-03-12 14:02:56 +0200

    fix AddMealController to spec

--- foodapp/src/com/candylife/controllers/AddMealConroller.java
+++ foodapp/src/com/candylife/controllers/AddMealConroller.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.candylife.constants.Servlet;
 import com.candylife.constants.WebPage;
 import com.candylife.model.Meal;
 import com.candylife.service.MealService;
@@ -20,9 +21,7 @@ public class AddMealConroller extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		//Maybe ControllerUtil.checkParam should be called from Service layer?
-		
+				
 		String title = ControllerUtil.checkParam(req.getParameter(WebPage.TITLE), WebPage.NO_TITLE);
 		String description = ControllerUtil.checkParam(req.getParameter(WebPage.DESCRIPTION), WebPage.NO_DESCRIPTION);
 		String type = ControllerUtil.checkParam(req.getParameter(WebPage.TYPE), WebPage.NO_TYPE);
@@ -32,22 +31,16 @@ public class AddMealConroller extends HttpServlet {
 		
 		Meal meal = new Meal(new MealBuilder(title, type, price).available(available).owner(owner).description(description));
 		
-		//String isAdd = MealService.add(title, description, type, available, price, owner);
+		boolean isAdd = MealService.add(meal);
 		
 		PrintWriter out = resp.getWriter();
 		
-		//out.println(isAdd);
-		
-//		if (isAdd) {
-//			//Constant Done!
-//			out.println(Servlet.ADD_SUCSEFULLY);
-//			
-//			//Util class to convert to html
-//			out.println("type: " + type + "title: " + title + "\ndescription: " + description + "\navailable: " + available + 
-//					"\nprice: " + price + "\nowner: " + owner + "\ntime: " + time);
-//		} else {
-//			//constant Done!
-//			out.println(Servlet.ADD_ERROR); //TODO: add error message
-//		}
+		if (isAdd) {
+			out.println(Servlet.ADD_SUCSEFULLY);
+			
+			out.println(meal.toString());
+		} else {
+			out.println(Servlet.ADD_ERROR); //TODO: add error message
+		}
 	}	
 }
