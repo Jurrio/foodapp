b8cc8e93c62a880fa285ca14dc31663c7c060c41
Jurrio <jurimik@yandex.ua>
2016-03-12 14:04:35 +0200

    remove unused imports and add value type in lists

--- foodapp/src/com/candylife/service/MealService.java
+++ foodapp/src/com/candylife/service/MealService.java
@@ -2,18 +2,15 @@ package com.candylife.service;
 
 import java.util.List;
 
-import com.candylife.constants.Servlet;
 import com.candylife.dao.MealRepository;
 import com.candylife.model.Meal;
-import com.candylife.util.CheckUtil;
-import com.candylife.util.MealUtil;
 
 public class MealService {
 	public static boolean add(Meal meal){
 		return new MealRepository().create(meal); 
 	}
 	
-	public static List find(String search) {
+	public static List<Meal> find(String search) {
 		return new MealRepository().read(search);
 	}
 
@@ -21,7 +18,7 @@ public class MealService {
 		return new MealRepository().delete(id);
 	}
 
-	public static List showAll() {
+	public static List<Meal> showAll() {
 		return new MealRepository().read();
 	}
 }
