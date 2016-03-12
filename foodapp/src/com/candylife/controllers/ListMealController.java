ad3d764d5a2021b6627ccc84d77c91d5a537fc80
Jurrio <jurimik@yandex.ua>
2016-03-12 14:34:47 +0200

    added fix ListMealController to spec

--- foodapp/src/com/candylife/controllers/ListMealController.java
+++ foodapp/src/com/candylife/controllers/ListMealController.java
@@ -2,6 +2,7 @@ package com.candylife.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.candylife.constants.Servlet;
+import com.candylife.model.Meal;
 import com.candylife.service.MealService;
+import com.candylife.util.OutUtil;
 
 @WebServlet (name = "ListMealServlet", urlPatterns = "/listMeal")
 public class ListMealController extends HttpServlet {
@@ -18,9 +22,13 @@ public class ListMealController extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		//Should get List
-		String allMeals = MealService.listAll();
+		List<Meal> allMeals = MealService.showAll();
 		
 		PrintWriter writer = resp.getWriter();
-		writer.println(allMeals);
+		if (allMeals.size() > 0) {
+			writer.println(OutUtil.printList(allMeals));
+		} else {
+			writer.println(Servlet.SEARCH_NULL);
+		}
 	}
 }
