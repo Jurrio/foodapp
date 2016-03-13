9bcaf7b32c814ce6bd7df7f73b5e07a4ad53f659
Jurrio <jurimik@yandex.ua>
2016-03-13 17:50:55 +0200

    added exceptions to project

---
+++ src/com/candylife/exception/SearchEmptyException.java
@@ -0,0 +1,5 @@
+package com.candylife.exception;
+
+public class SearchEmptyException extends Exception {
+	
+}
---
+++ src/com/candylife/exception/SearchManyParamException.java
@@ -0,0 +1,5 @@
+package com.candylife.exception;
+
+public class SearchManyParamException extends Exception {
+
+}
