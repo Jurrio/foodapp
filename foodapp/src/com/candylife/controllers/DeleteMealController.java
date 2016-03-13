1504be285d4ecbc39c958839ec4efeadcbd31165
Jurrio <jurimik@yandex.ua>
2016-03-13 14:41:05 +0200

    added check parameter(id) in DeleteMealController

--- foodapp/src/com/candylife/controllers/DeleteMealController.java
+++ foodapp/src/com/candylife/controllers/DeleteMealController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.candylife.constants.ServletConstant;
 import com.candylife.constants.RequestParam;
 import com.candylife.service.MealService;
-import com.candylife.util.ControllerUtil;
+import com.candylife.util.Parser;
 
 @WebServlet(name = "DeleteMealServlet", urlPatterns = "/deleteMeal")
 public class DeleteMealController extends HttpServlet {
@@ -20,15 +20,22 @@ public class DeleteMealController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		int id = ControllerUtil.checkId(req.getParameter(RequestParam.ID), RequestParam.NO_ID);
-		
-		boolean isDeleted = MealService.delete(id);
-		
 		PrintWriter out = resp.getWriter();
-		if (isDeleted) {
-			out.println(ServletConstant.DELETE_SUCSEFULLY);
-		} else {
+		
+		try {
+			int id = Parser.parseId(req.getParameter(RequestParam.ID));
+		
+			boolean isDeleted = MealService.delete(id);
+			
+			if (isDeleted) {
+				out.println(ServletConstant.DELETE_SUCSEFULLY);
+			} else {
+				out.println(ServletConstant.DELETE_ERROR);
+			}
+		} catch (NumberFormatException e) {
 			out.println(ServletConstant.DELETE_ERROR);
+		} catch (Exception e) {
+			out.println(ServletConstant.UNKNOWN_EXCEPTION);
 		}
 	}
 
