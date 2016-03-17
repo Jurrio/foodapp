4a6f60cccb41277af38808c1e66c4c9859e3badb
Jurrio <jurimik@yandex.ua>
2016-03-17 11:07:24 +0200

    added message in custom Exceptions

--- src/com/candylife/exception/SearchEmptyException.java
+++ src/com/candylife/exception/SearchEmptyException.java
@@ -1,5 +1,16 @@
 package com.candylife.exception;
 
+import com.candylife.constants.ExceptionMessage;
+
 public class SearchEmptyException extends Exception {
+	public SearchEmptyException() {
+		super();
+	}
+
+	@Override
+	public String getMessage() {
+		return ExceptionMessage.SEARCH_EMPTY_MESSAGE;
+	}
+	
 	
 }
--- src/com/candylife/exception/SearchManyParamException.java
+++ src/com/candylife/exception/SearchManyParamException.java
@@ -1,5 +1,14 @@
 package com.candylife.exception;
 
-public class SearchManyParamException extends Exception {
+import com.candylife.constants.ExceptionMessage;
 
+public class SearchManyParamException extends Exception {
+	public SearchManyParamException() {
+		super();
+	}
+
+	@Override
+	public String getMessage() {
+		return ExceptionMessage.SEARCH_MANY_PARAM_MESSAGE;
+	}
 }
