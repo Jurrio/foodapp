65a257a5efece2edb347b4f1fd131042a774efcd
Jurrio <jurimik@yandex.ua>
2016-03-22 13:56:32 +0200

    created HomePageController

--- src/com/candylife/controllers/HomePageController.java
+++ src/com/candylife/controllers/HomePageController.java
@@ -12,7 +12,8 @@ public class HomePageController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("add.jsp");
+		response.getWriter().append("Just a moment!");
+		response.sendRedirect("index.jsp");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
