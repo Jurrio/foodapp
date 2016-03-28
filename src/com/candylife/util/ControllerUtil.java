357f7ab5d9fdce67f892d6ec5f18218de4ecdc5e
Jurrio <jurimik@yandex.ua>
2016-03-28 08:04:30 +0300

    renamed args in method setAttributes

--- src/com/candylife/util/ControllerUtil.java
+++ src/com/candylife/util/ControllerUtil.java
@@ -5,8 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import com.candylife.constants.RequestParam;
 
 public class ControllerUtil {
-	public static void setAttributes(HttpServletRequest req, String attrOne, String attrTwo) {
-		req.setAttribute(RequestParam.SUCCESS, attrOne);
-		req.setAttribute(RequestParam.MESSAGE, attrTwo);
+	public static void setAttributes(HttpServletRequest req, String success, String message) {
+		req.setAttribute(RequestParam.SUCCESS, success);
+		req.setAttribute(RequestParam.MESSAGE, message);
 	}
 }
