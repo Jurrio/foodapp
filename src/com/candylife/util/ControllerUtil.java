5eb2d401eb6853de5d1a170604bc99e9a24e3522
Jurrio <jurimik@yandex.ua>
2016-06-23 21:03:23 +0300

    implemented logging

--- src/com/candylife/util/ControllerUtil.java
+++ src/com/candylife/util/ControllerUtil.java
@@ -2,11 +2,17 @@ package com.candylife.util;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
+
 import com.candylife.constants.RequestParam;
 
 public class ControllerUtil {
+
+	private static final Logger LOG = Logger.getLogger(ControllerUtil.class.getName());
+
 	public static void setAttributes(HttpServletRequest req, String success, String message) {
 		req.setAttribute(RequestParam.SUCCESS, success);
 		req.setAttribute(RequestParam.MESSAGE, message);
+		LOG.debug("Set message: " + message + ", success: " + success);
 	}
 }
