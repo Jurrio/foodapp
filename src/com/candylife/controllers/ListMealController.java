84f59661e7a5ba77e38db5d05f6b9d177c79ef1d
Jurrio <jurimik@yandex.ua>
2016-06-23 21:23:34 +0300

    implemented log4j

--- src/com/candylife/controllers/ListMealController.java
+++ src/com/candylife/controllers/ListMealController.java
@@ -3,12 +3,13 @@ package com.candylife.controllers;
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 
 import com.candylife.constants.ServletConstant;
 import com.candylife.model.Meal;
@@ -19,28 +20,25 @@ import com.candylife.util.ControllerUtil;
 public class ListMealController extends HttpServlet {
 
 	private static final long serialVersionUID = 2027448965537193995L;
+	private static Logger LOG = Logger.getLogger(ListMealController.class.getName());
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RequestDispatcher disp = req.getRequestDispatcher("dashboard.jsp");
 		ControllerUtil.setAttributes(req, ServletConstant.VOID, ServletConstant.VOID);
-		disp.forward(req, resp);
+		req.getRequestDispatcher("dashboard.jsp").forward(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RequestDispatcher disp = req.getRequestDispatcher("dashboard.jsp");
 		List<Meal> allMeals = MealService.showAll();
 		
 		req.setAttribute("meals", allMeals);
-		
+		LOG.debug("size of allMeals: " + allMeals.size());
 		if (!allMeals.isEmpty()) {
 			ControllerUtil.setAttributes(req, ServletConstant.YES, ServletConstant.DISPLAY_ALL);
-			disp.forward(req, resp);
 		} else {
 			ControllerUtil.setAttributes(req, ServletConstant.NO, ServletConstant.EMPTY_SET);
-			disp.forward(req, resp);
-//			writer.println(ServletConstant.EMPTY_SET);
 		}
+		req.getRequestDispatcher("dashboard.jsp").forward(req, resp);
 	}
 }
