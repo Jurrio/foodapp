e6482aa8b1d48eac7ca879dbdaa9fffbca1821ec
Jurrio <jurimik@yandex.ua>
2016-06-24 23:50:43 +0300

    implemented PersonBuilder, UserBuilder

---
+++ src/com/candylife/builder/PersonBuilder.java
@@ -0,0 +1,38 @@
+package com.candylife.builder;
+
+import com.candylife.model.Person;
+import com.candylife.model.User;
+
+public class PersonBuilder {
+
+	private User user;
+	private String firstName;
+	private String lastName;
+
+	public PersonBuilder() {
+	}
+
+	public PersonBuilder user(User user) {
+		this.user = user;
+		return this;
+	}
+
+	public PersonBuilder firstName(String firstName) {
+		this.firstName = firstName;
+		return this;
+	}
+
+	public PersonBuilder lastName(String lastName) {
+		this.lastName = lastName;
+		return this;
+	}
+
+	public Person build() {
+		Person person = new Person();
+		person.setFirstName(firstName);
+		person.setLastName(lastName);
+		person.setUser(user);
+		return person;
+	}
+
+}
