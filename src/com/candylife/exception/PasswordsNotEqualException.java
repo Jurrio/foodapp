4a0ab3299f5e3a595e8f7bc27d15da162da51d24
Jurrio <jurimik@yandex.ua>
2016-06-25 00:12:48 +0300

    small fix

--- src/com/candylife/exception/PasswordsNotEqualException.java
+++ src/com/candylife/exception/PasswordsNotEqualException.java
@@ -4,6 +4,8 @@ import com.candylife.constants.ExceptionMessage;
 
 public class PasswordsNotEqualException extends Exception {
 
+	private static final long serialVersionUID = -4859304523650555818L;
+
 	public PasswordsNotEqualException() {
 		super();
 	}
