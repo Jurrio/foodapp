10acf4051e0690e1666af2d42b6e3c71493917bd
Jurrio <jurimik@yandex.ua>
2016-06-26 20:29:50 +0300

    updated custom exception

--- src/com/candylife/exception/PasswordsNotEqualException.java
+++ src/com/candylife/exception/PasswordsNotEqualException.java
@@ -1,7 +1,5 @@
 package com.candylife.exception;
 
-import com.candylife.constants.ExceptionMessage;
-
 public class PasswordsNotEqualException extends Exception {
 
 	private static final long serialVersionUID = -4859304523650555818L;
@@ -13,12 +11,4 @@ public class PasswordsNotEqualException extends Exception {
 	public PasswordsNotEqualException(String message) {
 		super(message);
 	}
-
-	@Override
-	public String getMessage() {
-		return ExceptionMessage.PASSWORDS_DO_NOT_MATCH;
-	}
-	
-	
-
 }
