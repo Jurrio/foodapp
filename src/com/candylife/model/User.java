50074e2939c61b1e11b80129b3860ed71e952447
Jurrio <jurimik@yandex.ua>
2016-06-27 13:38:41 +0300

    overrided methods equals and hashcode

--- src/com/candylife/model/User.java
+++ src/com/candylife/model/User.java
@@ -42,4 +42,25 @@ public class User {
 		this.isChef = isChef;
 	}
 
+	@Override
+	public int hashCode() {
+		return 31 * email.hashCode() + 7 * password.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (!this.getEmail().equals(other.getEmail()))
+			return false;
+		if (!this.getPassword().equals(other.getPassword()))
+			return false;
+		return true;
+	}
+
 }
