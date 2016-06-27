5b064de7a04eb2114b601e166d9f6d18f0cb7a2c
Jurrio <jurimik@yandex.ua>
2016-06-27 17:58:52 +0300

    deleted scr-old
    //oooooops )))

--- src-old/com/candylife/constants/ExceptionMessage.java
+++
@@ -1,7 +0,0 @@
-package com.candylife.constants;
-
-public class ExceptionMessage {
-	public static final String SEARCH_MANY_PARAM_MESSAGE = "Unfortunally, our service is very lazy and does not seek more than one word.";
-	public static final String SEARCH_EMPTY_MESSAGE = "You tried use search with empty query. Do not do it this way! ;-)";
-	public static final String PASSWORDS_DO_NOT_MATCH = "Passwords do not match!";
-}
--- src-old/com/candylife/enums/Type.java
+++
@@ -1,8 +0,0 @@
-/*package com.candylife.enums;
-
-public enum Type {
-	SOUP,
-	MAIN_COURSE,
-	DESERT
-}
-*/
