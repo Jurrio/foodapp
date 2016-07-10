d096bd684ddc74a01515acca2c15a00ca5b7adf6
Jurrio <jurimik@yandex.ua>
2016-07-10 08:18:10 +0300

    added id for UserBuilder

--- src/main/java/builder/UserBuilder.java
+++ src/main/java/builder/UserBuilder.java
@@ -7,15 +7,22 @@ public class UserBuilder {
 	private final String email;
 	private final String password;
 	private boolean isChef;
+	private int id;
 
 	public UserBuilder(String email, String password) {
 		this.email = email;
 		this.password = password;
 		this.isChef = false;
+		this.id = 0;
 	}
 
 	public UserBuilder chef(boolean isChef) {
 		this.isChef = isChef;
+		return this;
+	}
+	
+	public UserBuilder id(int id) {
+		this.id = id;
 		return this;
 	}
 
@@ -24,6 +31,7 @@ public class UserBuilder {
 		user.setEmail(email);
 		user.setPassword(password);
 		user.setChef(isChef);
+		user.setId(id);
 		return user;
 	}
 }
