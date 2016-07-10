6776465ad765022074fb35a8287ed2c9fe5ca618
Jurrio <jurimik@yandex.ua>
2016-07-10 09:10:04 +0300

    added exceptions
    SignInException - for login
    StringFormatException - for String which is converted into Person

---
+++ src/main/java/exception/SignInException.java
@@ -0,0 +1,11 @@
+package main.java.exception;
+
+public class SignInException extends Exception {
+
+	private static final long serialVersionUID = 817911006037137487L;
+
+	public SignInException(String message) {
+		super(message);
+	}
+	
+}
---
+++ src/main/java/exception/StringFormatException.java
@@ -0,0 +1,11 @@
+package main.java.exception;
+
+public class StringFormatException extends Exception {
+
+	private static final long serialVersionUID = 4048304246717522069L;
+
+	public StringFormatException(String message) {
+		super(message);
+	}
+
+}
