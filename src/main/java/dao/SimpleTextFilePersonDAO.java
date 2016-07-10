547d6906f38334b1eb9c39d6ef8461ee4b712cd9
Jurrio <jurimik@yandex.ua>
2016-07-10 09:16:09 +0300

    created SimpleTextFilePersonDAO instead of PersonFileDAO

---
+++ src/main/java/dao/SimpleTextFilePersonDAO.java
@@ -0,0 +1,117 @@
+package main.java.dao;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import main.java.builder.UserBuilder;
+import main.java.exception.SignInException;
+import main.java.exception.StringFormatException;
+import main.java.model.Person;
+import main.java.model.User;
+import main.java.util.FileUtils;
+import main.java.util.PersonUtil;
+
+public class SimpleTextFilePersonDAO implements AbstractPersonFileDAO {
+	private static File storage;
+
+	@Override
+	public void add(Person person) throws FileNotFoundException, IOException {
+		storage = FileUtils.getFile("persons.txt");
+		BufferedWriter writer = new BufferedWriter(new FileWriter(storage));
+		try {
+			writer.write(person.toString());
+		} finally {
+			writer.close();
+		}
+	}
+
+	@Override
+	public Person login(String login, String password)
+			throws FileNotFoundException, IOException, StringFormatException, SignInException {
+		User usr = new UserBuilder(login, password).build();
+		Person person = null;
+
+		BufferedReader reader = new BufferedReader(new FileReader(storage));
+		try {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				if (line.contains(login) && line.contains(password)) {
+					person = convertFromString(line);
+					if (usr.equals(person.getUser())) {
+						reader.close();
+						return person;
+					}
+				}
+			}
+		} finally {
+			reader.close();
+		}
+
+		throw new SignInException("You've entered an invalid email address / password combination.");
+	}
+
+	@Override
+	public Person get(int id) throws FileNotFoundException, IOException, StringFormatException {
+		BufferedReader reader = new BufferedReader(new FileReader(storage));
+		try {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				Person person = convertFromString(line);
+				if (person.getId() == id) {
+					reader.close();
+					return person;
+				}
+			}
+		} finally {
+			reader.close();
+		}
+		return null;
+	}
+
+	@Override
+	public void update(int id, Person person) {
+		// TODO not implemented method
+	}
+
+	@Override
+	public boolean delete(Person deletedPerson) throws FileNotFoundException, IOException, StringFormatException {
+		File tempFile = FileUtils.getFile("tempFile");
+
+		BufferedReader reader = new BufferedReader(new FileReader(storage));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+		boolean isSuccessful;
+		try {
+			String line;
+			while ((line = reader.readLine()) != null) {
+
+				Person person = convertFromString(line);
+
+				if (!deletedPerson.getUser().equals(person.getUser())) {
+					writer.write(line + "\n");
+				}
+			}
+			isSuccessful = tempFile.renameTo(storage);
+		} finally {
+			reader.close();
+			writer.close();
+		}
+
+		return isSuccessful;
+	}
+
+	@Override
+	public String convertToString(Person person) {
+		return person.toString();
+	}
+
+	@Override
+	public Person convertFromString(String string) throws StringFormatException {
+		return PersonUtil.valueOfSimpleString(string);
+	}
+
+}
