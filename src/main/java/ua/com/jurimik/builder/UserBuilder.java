911b139d01dd75ef7b6b55cdd6d55ec8b4734cbe
Jurrio <jurimik@yandex.ua>
2016-07-21 21:51:07 +0300

    added boolean isAdmin in UserBuilder

--- src/main/java/ua/com/jurimik/builder/UserBuilder.java
+++ src/main/java/ua/com/jurimik/builder/UserBuilder.java
@@ -7,17 +7,24 @@ public class UserBuilder {
 	private final String email;
 	private final String password;
 	private boolean isChef;
+	private boolean isAdmin;
 	private int id;
 
 	public UserBuilder(String email, String password) {
 		this.email = email;
 		this.password = password;
 		this.isChef = false;
+		this.isAdmin = false;
 		this.id = 0;
 	}
 
 	public UserBuilder chef(boolean isChef) {
 		this.isChef = isChef;
+		return this;
+	}
+	
+	public UserBuilder admin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
 		return this;
 	}
 
@@ -31,6 +38,7 @@ public class UserBuilder {
 		user.setEmail(email);
 		user.setPassword(password);
 		user.setChef(isChef);
+		user.setAdmin(isAdmin);
 		user.setId(id);
 		return user;
 	}
