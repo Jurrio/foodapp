cff4bb2bc0b6346b7b9377dd76c3e953b7800efd
Jurrio <jurimik@yandex.ua>
2016-08-17 14:25:25 +0300

    added default type() for annotation

--- src/main/java/ua/com/jurimik/annotation/ShutdownBackupRestore.java
+++ src/main/java/ua/com/jurimik/annotation/ShutdownBackupRestore.java
@@ -5,8 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import ua.com.jurimik.enums.SerializationType;
+
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ShutdownBackupRestore {
-	
+	SerializationType type() default SerializationType.BYTE_STREAM;
 }
